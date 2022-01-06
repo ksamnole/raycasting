d6a1e27f72d87d7a82d57cd4074a9fb6565c80ac
Maxim <atm.tsvetk@gmail.com>
2022-01-06 19:55:03 +0500

    feat: display sphere in space

--- RayCasting/Window.cs
+++ RayCasting/Window.cs
@@ -27,11 +27,42 @@ namespace RayCasting
             GL.ClearColor(OpenTK.Graphics.Color4.CornflowerBlue);
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
+            var cameraOrigin = new Vector3(0.0f, 0.0f, -1.5f);
+            GL.Begin(PrimitiveType.Points);
+            for (var x = 0; x < Width; x++)
+                for (int y = 0; y < Height; y++)
+                {
+                    var _x = (2 * x - Width) / (float)Width;
+                    var _y = (2 * y - Height) / (float)Height;
+                    var cameraDirection = Vector3.Normalize(new Vector3(_x, _y, 1.0f));
+                    var col = CastRay(cameraOrigin, cameraDirection);
+                    col.X = (float)Math.Pow(col.X, 0.45);
+                    col.Y = (float)Math.Pow(col.Y, 0.45);
+                    col.Z = (float)Math.Pow(col.Z, 0.45);
+                    GL.Color3(col);
+                    GL.Vertex3(_x, _y, 0);
+                }
+            GL.End();
+
+            rotation += 0.5f;
+
             Context.SwapBuffers();
             base.OnRenderFrame(e);
         }
 
-        private float PlateIntersect(Vector3 origin, Vector3 direction, Vector4 p)
+        private Vector3 CastRay(Vector3 origin, Vector3 direction)
+        {
+            var spherePos = new Vector3(0.0f, 0.0f, 0.0f);
+            var it = SphereIntersect(origin - spherePos, direction, 0.5f);
+            if (it.X < 0.0)
+                return new Vector3(0.3f, 0.6f, 1.0f);
+            var itPos = origin + direction * it.X;
+            var light = Vector3.Normalize(new Vector3((float)Math.Cos(rotation), 0.75f, -0.5f));
+            var diffuse = Math.Max(0.0f, Vector3.Dot(light, itPos)) * 0.5f + 0.1f;
+            return new Vector3(diffuse);
+        }
+
+        private float PlaneIntersect(Vector3 origin, Vector3 direction, Vector4 p)
         {
             return -(Vector3.Dot(origin, p.Xyz) + p.W) / Vector3.Dot(direction, p.Xyz);
         }
