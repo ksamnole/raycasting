22fa83a43cf91a12cbfd3749d6c1f44cdec90ceb
Maxim <atm.tsvetk@gmail.com>
2022-01-06 20:00:25 +0500

    refactor: add comments(russian)

--- RayCasting/Window.cs
+++ RayCasting/Window.cs
@@ -29,16 +29,22 @@ namespace RayCasting
 
             var cameraOrigin = new Vector3(0.0f, 0.0f, -1.5f);
             GL.Begin(PrimitiveType.Points);
+            // Пускаем лучи в каждый пиксель экрана
             for (var x = 0; x < Width; x++)
                 for (int y = 0; y < Height; y++)
                 {
+                    // Берем координаты в нашем окне. Окно от -1 до 1, в то время как пиксели от 0 до ???
                     var _x = (2 * x - Width) / (float)Width;
                     var _y = (2 * y - Height) / (float)Height;
+                    // Направление камеры
                     var cameraDirection = Vector3.Normalize(new Vector3(_x, _y, 1.0f));
+                    // Цвет пикселя. Если попал в объект, то цвет объекта, иначе цвет неба.
                     var col = CastRay(cameraOrigin, cameraDirection);
+                    // Цвета будут более реалистичные
                     col.X = (float)Math.Pow(col.X, 0.45);
                     col.Y = (float)Math.Pow(col.Y, 0.45);
                     col.Z = (float)Math.Pow(col.Z, 0.45);
+                    // Красим пиксель
                     GL.Color3(col);
                     GL.Vertex3(_x, _y, 0);
                 }
@@ -54,21 +60,26 @@ namespace RayCasting
         {
             var spherePos = new Vector3(0.0f, 0.0f, 0.0f);
             var it = SphereIntersect(origin - spherePos, direction, 0.5f);
+            // Проверка попал ли луч в объект, если нет то возвращаем цвет неба.
             if (it.X < 0.0)
                 return new Vector3(0.3f, 0.6f, 1.0f);
             var itPos = origin + direction * it.X;
+            // Направление освещения
             var light = Vector3.Normalize(new Vector3((float)Math.Cos(rotation), 0.75f, -0.5f));
+            // Расчет освещения
             var diffuse = Math.Max(0.0f, Vector3.Dot(light, itPos)) * 0.5f + 0.1f;
             return new Vector3(diffuse);
         }
 
         private float PlaneIntersect(Vector3 origin, Vector3 direction, Vector4 p)
         {
+            // Метод пересечения луча с плоскостью
             return -(Vector3.Dot(origin, p.Xyz) + p.W) / Vector3.Dot(direction, p.Xyz);
         }
 
         private Vector2 SphereIntersect(Vector3 origin, Vector3 direction, float radius)
         {
+            // Метод пересечения луча со сферой
             var b = Vector3.Dot(origin, direction);
             var c = Vector3.Dot(origin, origin) - radius * radius;
             var h = b * b - c;
