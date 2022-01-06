bf6e537e4b4f0c4ce4557ac8c5d927ff973601a4
Maxim <atm.tsvetk@gmail.com>
2022-01-06 19:46:31 +0500

    feat: create opentk gamewindow

---
+++ RayCasting/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RayCasting
+{
+    class Program
+    {
+        static void Main()
+        {
+            var window = new Window(720, 720);
+            window.VSync = OpenTK.VSyncMode.Off;
+            window.Run(60, 60);
+        }
+    }
+}
---
+++ RayCasting/Window.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Timers;
+using System.Windows;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace RayCasting
+{
+    public class Window : GameWindow
+    {
+        public Window(int width, int height) : base(width, height) { }
+    }
+}
